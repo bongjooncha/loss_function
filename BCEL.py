c4b31b73b3292c6ef593f3af041ed87e84b9bffd
hiddenbebb <cbj4133@naver.com>
2024-02-04 21:47:10 +0900

    bcel

--- BCEL.py
+++ BCEL.py
@@ -1,9 +1,12 @@
 #Binary Cross-Entropy Loss
+import torch
+
+def binary_cross_entropy_loss(y_true, y_pred):
+    loss = - (y_true * torch.log(y_pred) + (1 - y_true) * torch.log(1 - y_pred))
+    return torch.mean(loss)
+
 
 import torch.nn.functional as F
 
 def binary_cross_entropy_loss(y_true, y_pred):
     return F.binary_cross_entropy_with_logits(y_pred, y_true)
-
-# 사용 예시:
-# loss = binary_cross_entropy_loss(y_true, y_pred)
